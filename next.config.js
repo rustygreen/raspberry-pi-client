5244442c56dd9b4f722df3da3f7919732c5ad08c
Russell Green <russell.green@rizing.com>
2023-06-20 08:44:34 -0400

    chore: updates

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const { env } = require("process");
 const { PHASE_PRODUCTION_SERVER } = require("next/constants");
 const CONFIG_ENV_VAR_NAME = "RPC_CONFIG";
-const CONFIG_JSON = env[CONFIG_ENV_VAR_NAME];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,8 +11,16 @@ const nextConfig = {
 
 module.exports = (phase) => {
   const isProduction = phase === PHASE_PRODUCTION_SERVER;
-  const saveConfigFile = isProduction && Boolean(CONFIG_JSON);
+  const configJson = env[CONFIG_ENV_VAR_NAME];
+  const hasConfig = Boolean(configJson);
+  const saveConfigFile = isProduction && hasConfig;
 
+  const mode = isProduction ? "production" : "development";
+  const configInfo = hasConfig
+    ? "No config env variable found."
+    : "Config env variable exists.";
+
+  console.log(`Initializing app in '${mode}' mode. ${configInfo}`);
   if (saveConfigFile) {
     saveConfig(config);
   }
@@ -26,5 +33,6 @@ function saveConfig() {
     `Saving config file from '${CONFIG_ENV_VAR_NAME}' env variable value`
   );
 
-  fs.writeFileSync("./data/config.json", CONFIG_JSON);
+  const configJson = env[CONFIG_ENV_VAR_NAME];
+  fs.writeFileSync("./data/config.json", configJson);
 }
